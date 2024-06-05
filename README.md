f41afe2feee8668d30da2a712475817676a5bc08
hritikkrishangupta <hritikkrishangupta@gmail.com>
2024-06-05 19:02:25 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b3c5944592a388cbe495319e483d4c1a9d43ba01ebd26bcbbd12ab3edc40486d
+size 32
---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:465ac1415e09a65b474ce35fe9887947911aa92c78173ab59dc4afc54d14b051
+size 1823
